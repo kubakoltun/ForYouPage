fa7d1d94439cf426a2173dfc184b9baed5eb55eb
kubakoltun <kubakoltun1232@gmail.com>
2023-04-28 12:50:10 +0200

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const url = 'mongodb://localhost:27017/mydb';
+const url = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log('Database created!');
-});
+const client = new MongoClient(url, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+// MongoClient.connect(url)
+//     .then(client => {
+//         console.log('Connected');
+//         const db = client.db('foryoudb')
+//         const postsCollection = db.collection('posts')
+//     })
 
 app.listen(8000, () => {
     console.log('Server running on port 8000');
